21444e50dcfdb1c25160d5e47c3aadcdd8e188a1
Sai Somanath.K <k.saisomanath@gmail.com>
2017-12-07 21:40:13 +0530

    Increase readability

--- socket_client.c
+++ socket_client.c
@@ -3,25 +3,24 @@
 
 int main(int argc, char *argv[]) {
   char fname[50], buffer[1025];
-  int sd, n;
+  int sockfd, n;
   struct sockaddr_in address;
 
   address.sin_family = AF_INET;
   address.sin_port = htons(15000);
   address.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-  if ((sd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    perror("socket");
-  connect(sd, (struct sockaddr *)&address, sizeof(address));
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  connect(sockfd, (struct sockaddr *)&address, sizeof(address));
 
   printf("Enter a filename: ");
   scanf("%s", fname);
 
   printf("Sending request... \n");
-  send(sd, fname, sizeof(fname), 0); // send a message on a socket
+  send(sockfd, fname, sizeof(fname), 0); // send a message on a socket
 
   printf("Recieved response: \n");
-  while ((n = recv(sd, buffer, sizeof(buffer), 0)) > 0)
+  while ((n = recv(sockfd, buffer, sizeof(buffer), 0)) > 0)
     write(1, buffer, n);
 
   return 0;
